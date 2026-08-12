79f6675ef4be2ef37837eacd8524ccefda7a4b3e
Dmitry <banindm@email.ru>
2026-08-12 14:04:20 +0300

    Изменены иконки, добавлены контакты в файл. Добавлены файлы политики

---
+++ src/config/contacts.js
@@ -0,0 +1,12 @@
+export const contacts = {
+  phone: {
+    display: '+7 915 374 75 56',
+    href: 'tel:+79153747556',
+  },
+  telegram: 'https://t.me/rgballet',
+  max: 'https://max.ru/u/f9LHodD0cOI2YW1eR59U_XysQrHY4zQOnF-1q6Pl__EFNVEJoY0miBfMIbM',
+  email: {
+    display: 'rg-ballet@mail.ru',
+    href: 'mailto:rg-ballet@mail.ru',
+  },
+};
