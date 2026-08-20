c2b7eae67803286fc1a04e77bcf13cfa08e1e5cf
Dmitry <banindm@mail.ru>
2026-08-20 23:58:04 +0300

    Настроен тестовый деплой на GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
 
 export default defineConfig({
+  site: 'https://dmitrybanin.github.io',
+  base: '/RGballet',
   devToolbar: {
     enabled: false,
   },
