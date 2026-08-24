b4dcc1a68bc425eb644402510eba545fad14bbad
Dmitry <banindm@mail.ru>
2026-08-24 22:47:58 +0300

    Комментарии

--- src/config/contacts.js
+++ src/config/contacts.js
@@ -13,6 +13,8 @@ export const contacts = {
   address: 'Октябрьский проспект 186 2-й этаж',
   map: {
     desktop: 'https://yandex.ru/maps/-/CTwKmL7r',
+    reviews:
+      'https://yandex.ru/maps/org/rg_ballet/218552276510/reviews/?ll=37.894568%2C55.677712&utm_source=share&z=16',
     android:
       'geo:0,0?q=%D0%9B%D1%8E%D0%B1%D0%B5%D1%80%D1%86%D1%8B%2C%20%D0%9E%D0%BA%D1%82%D1%8F%D0%B1%D1%80%D1%8C%D1%81%D0%BA%D0%B8%D0%B9%20%D0%BF%D1%80%D0%BE%D1%81%D0%BF%D0%B5%D0%BA%D1%82%2C%20186',
     ios: 'https://maps.apple.com/?q=%D0%9B%D1%8E%D0%B1%D0%B5%D1%80%D1%86%D1%8B%2C%20%D0%9E%D0%BA%D1%82%D1%8F%D0%B1%D1%80%D1%8C%D1%81%D0%BA%D0%B8%D0%B9%20%D0%BF%D1%80%D0%BE%D1%81%D0%BF%D0%B5%D0%BA%D1%82%2C%20186',
